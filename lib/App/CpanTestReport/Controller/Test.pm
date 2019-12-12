1a56cb90474d5024e41072cfc6d0ef0076345daa
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-12-12 21:30:22 +0100

    Fix parsing new email format

--- lib/App/CpanTestReport/Controller/Test.pm
+++ lib/App/CpanTestReport/Controller/Test.pm
@@ -5,8 +5,9 @@ sub report {
   my $self = shift;
 
   return $self->_get_report_p($self->stash('guid'))->then(sub {
-    my $res = shift;
-    $self->respond_to(json => {json => $res}, any => $res);
+    my $report = shift;
+    my $text   = delete $report->{result}{output}{uncategorized};
+    $self->respond_to(json => {json => $report}, txt => {text => $text}, any => $report);
   });
 }
 
@@ -24,18 +25,33 @@ sub _get_report_p {
 
 sub _parse_report {
   my ($self, $report) = @_;
-  my @lines = split /\n\r?/, $report->{result}{output}{uncategorized} || '';
+  my @lines   = split /\n\r?/, $report->{result}{output}{uncategorized} || '';
   my $section = 'preamble';
   my $sub_section;
 
+  $report->{result}{environment_and_other_context} //= '';
+  $report->{result}{preamble}                      //= '';
+  $report->{result}{prerequisites} = {};
+  $report->{result}{program_output}  //= '';
+  $report->{result}{tester_comments} //= '';
+
   while (defined(my $line = shift @lines)) {
     if ($line =~ /^Sections of this report/) {
-      $report->{result}{output}{$section} ||= _clean($report->{block});
+      $report->{result}{$section} ||= _clean($report->{block});
       $report->{block} = '';
       $section = 'toc';
     }
+    elsif ($line =~ /^PREREQUISITES/i) {
+      $report->{result}{$section} ||= _clean($report->{block});
+      $section = 'prerequisites';
+    }
+    elsif ($line =~ /^TEST RESULTS/i) {
+      $report->{result}{$section} ||= _clean($report->{block});
+      $section = 'program_output';
+      $report->{block} = '';
+    }
     elsif ($line =~ /^-----+$/ and $lines[1] eq $line) {
-      $report->{result}{output}{$section} ||= _clean($report->{block});
+      $report->{result}{$section} ||= _clean($report->{block});
       $report->{block} = '';
       local $_ = shift @lines;
       next unless /(\w.*)/;
@@ -44,20 +60,24 @@ sub _parse_report {
     }
     elsif ($section eq 'prerequisites') {
       $sub_section = $1 if $line =~ /^(\w+):/;
-      $report->{result}{output}{$section}{$sub_section}{$1} = $2
-        if $sub_section and $line =~ m!(\S+)\s+([\d\.]+)\s+([\d\.]+)!;
+      $sub_section ||= 'requires';
+      $report->{result}{$section}{$sub_section}{$1} = $2
+        if $line =~ m!(\S+)\s+([\d\.]+)\s+([\d\.]+)! or $line =~ m!(\S+)\s+([\d\.]+)!;
     }
     else {
       $report->{block} .= "$line\n";
     }
+
+    # warn "--- $section <<< ($line)\n";
   }
 
+  #warn Mojo::Util::dumper($report);
+
   $report->{reporter}{email} ||= '';
-  $report->{reporter}{email} =~ s!\s(.*)!!;
-  $report->{result}{output}{tester_comments} =~ s!Additional comments from \S+!!;
-  $report->{result}{output}{$section} = _clean($report->{block});    # Capture last block
+  $report->{reporter}{email}         =~ s!\s(.*)!!;
+  $report->{result}{tester_comments} =~ s!Additional comments from \S+!!;
+  $report->{result}{$section} ||= _clean($report->{block});    # Capture last block
   delete $report->{block};
-  delete $report->{result}{output}{uncategorized};
   return $report;
 }
 
